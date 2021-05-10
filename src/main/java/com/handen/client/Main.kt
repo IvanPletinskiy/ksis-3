0e3a1b38d7858d3be4818217a3e0f3f25c9153ed
Ivan Pletinskiy <ivanhanden@gmail.com>
2021-05-10 12:56:53 +0300

    finish ksis-3

--- src/main/java/com/handen/client/Main.kt
+++ src/main/java/com/handen/client/Main.kt
@@ -1,21 +1,95 @@
 package com.handen.client
 
+import org.apache.http.client.methods.CloseableHttpResponse
 import org.apache.http.client.methods.RequestBuilder
 import org.apache.http.impl.client.HttpClients
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import java.net.URI
 import java.nio.charset.Charset
 
 fun main() {
-    val fooUrl = "https://0.0.0.0:1990/files?src=C:/ksis/src.txt&dst=C:/ksis/dst.txt"
+    val bufferedReader = BufferedReader(InputStreamReader(System.`in`))
+    while (true) {
+        println("Enter one of actions: GET, PUT, POST, DELETE, COPY, MOVE")
+        val line = bufferedReader.readLine()
+        when (line) {
+            "GET" -> processGet(bufferedReader)
+            "PUT" -> processPut(bufferedReader)
+            "POST" -> processPost(bufferedReader)
+            "DELETE" -> processDelete(bufferedReader)
+            "COPY" -> processCopy(bufferedReader)
+            "MOVE" -> processMove(bufferedReader)
+            else -> println("Unknown action")
+        }
+    }
+}
+
+fun processGet(bufferedReader: BufferedReader) {
+    println("ENTER src:")
+    val src = bufferedReader.readLine()
+    sendRequest("GET", "src=$src")
+}
+
+fun processPut(bufferedReader: BufferedReader) {
+    println("ENTER src:")
+    val src = bufferedReader.readLine()
+    println("ENTER text:")
+    val text = bufferedReader.readLine()
+    sendRequest("PUT", "src=$src", "text=$text")
+}
+
+fun processPost(bufferedReader: BufferedReader) {
+    println("ENTER src:")
+    val src = bufferedReader.readLine()
+    println("ENTER text:")
+    val text = bufferedReader.readLine()
+    sendRequest("POST", "src=$src", "text=$text")
+}
+
+fun processDelete(bufferedReader: BufferedReader) {
+    println("ENTER src:")
+    val src = bufferedReader.readLine()
+    sendRequest("DELETE", "src=$src")
+}
+
+fun processCopy(bufferedReader: BufferedReader) {
+    println("ENTER src:")
+    val src = bufferedReader.readLine()
+    println("ENTER dst:")
+    val dst = bufferedReader.readLine()
+    sendRequest("COPY", "src=$src", "dst=$dst")
+}
+
+fun processMove(bufferedReader: BufferedReader) {
+    println("ENTER src:")
+    val src = bufferedReader.readLine()
+    println("ENTER dst:")
+    val dst = bufferedReader.readLine()
+    sendRequest("MOVE", "src=$src", "dst=$dst")
+}
+
+fun sendRequest(method: String, param1: String, param2: String? = null) {
     val httpClient = HttpClients.createDefault()
-//    val request = RequestBuilder.create("COPY").setCharset(Charsets.UTF_16).setUri(URI.create("https://0.0.0.0:1990/files?src=C:/ksis/src.txt&dst=C:/ksis/dst.txt")).build()
-//    val request = RequestBuilder.create("COPY").setCharset(Charsets.UTF_16).setUri(URI.create("https://0.0.0.0:1990/files")).build()
-    val request = RequestBuilder.create("GET")
+    var uri = "http://localhost:8000/files?$param1"
+    param2?.let {
+        uri += "&$param2"
+    }
+    val request = RequestBuilder.create(method)
         .setCharset(Charset.forName("UTF-8"))
-        .setUri(URI.create("http://localhost:8000/test?src=C:/ksis/src.txt")).build()
+        .setUri(URI.create(uri)).build()
 
     val response = httpClient.execute(request)
-    if(response.statusLine.statusCode == 200) {
-        println(response.entity.content.bufferedReader().readText())
+
+    handleResponse(response)
+}
+
+fun handleResponse(response: CloseableHttpResponse) {
+    println("Code: ${response.statusLine.statusCode}")
+    if(response.entity.contentLength > 0) {
+        val body = response.entity.content.bufferedReader().readText()
+        if (body.isNotEmpty()) {
+            println(body)
+        }
     }
 }
--- src/main/java/com/handen/server/Main.kt
+++ src/main/java/com/handen/server/Main.kt
@@ -5,221 +5,164 @@ import com.sun.net.httpserver.HttpHandler
 import com.sun.net.httpserver.HttpServer
 import java.io.File
 import java.net.InetSocketAddress
-
-
-//private val connections = mutableListOf<ConnectionHandler>()
-//
-//typealias OnMessageReceived = (ConnectionHandler, String) -> Unit
-//
-////@Throws(Exception::class)
-////@JvmStatic
-//fun main() {
-//
-//    // création de la socket
-//    val port = 1990
-//    val serverSocket = ServerSocket(port)
-//    System.err.println("Serveur lancé sur le port : $port")
-//
-//    // repeatedly wait for connections, and process
-//    while (true) {
-//
-//        // on reste bloqué sur l'attente d'une demande client
-//        val clientSocket = serverSocket.accept()
-//        System.err.println("Nouveau client connecté")
-//
-//        // on ouvre un flux de converation
-//
-////        val `in` = BufferedReader(
-////            InputStreamReader(clientSocket.getInputStream(), "utf16")
-////        )
-//        val out = PrintWriter(
-//            BufferedWriter(
-//                OutputStreamWriter(clientSocket.getOutputStream())
-//            ),
-//            true
-//        )
-//
-//        val inputStream = clientSocket.getInputStream()
-//        val bufferedInputReader = inputStream.bufferedReader(Charsets.UTF_16)
-//        val text = bufferedInputReader.readLines()
-//        println(text)
-//
-//        // chaque fois qu'une donnée est lue sur le réseau on la renvoi sur le flux d'écriture.
-//        // la donnée lue est donc retournée exactement au même client.
-////        var s: String
-////        `in`.lines().forEach {
-////            println(it)
-//
-////            out.write("HTTP/1.0 200 OK\r\n")
-////            out.write("Date: Fri, 31 Dec 1999 23:59:59 GMT\r\n")
-////            out.write("Server: Apache/0.8.4\r\n")
-////            out.write("Content-Type: text/html\r\n")
-////            out.write("Content-Length: 59\r\n")
-////            out.write("Expires: Sat, 01 Jan 2000 00:59:59 GMT\r\n")
-////            out.write("Last-modified: Fri, 09 Aug 1996 14:21:40 GMT\r\n")
-////            out.write("\r\n")
-////            out.write("<TITLE>Exemple</TITLE>")
-////            out.write("<P>Ceci est une page d'exemple.</P>")
-////        while ((s = `in`.readLine()) != null) {
-////            println(s)
-////
-////
-////            out.write("HTTP/1.0 200 OK\r\n")
-////            out.write("Date: Fri, 31 Dec 1999 23:59:59 GMT\r\n")
-////            out.write("Server: Apache/0.8.4\r\n")
-////            out.write("Content-Type: text/html\r\n")
-////            out.write("Content-Length: 59\r\n")
-////            out.write("Expires: Sat, 01 Jan 2000 00:59:59 GMT\r\n")
-////            out.write("Last-modified: Fri, 09 Aug 1996 14:21:40 GMT\r\n")
-////            out.write("\r\n")
-////            out.write("<TITLE>Exemple</TITLE>")
-////            out.write("<P>Ceci est une page d'exemple.</P>")
-////        }
-//
-//        // on ferme les flux.
-//        System.err.println("Connexion avec le client terminée")
-//        out.close()
-////    `in`.close()
-//        bufferedInputReader.close()
-//        clientSocket.close()
-//    }
-//}
-//
-//fun main() {
-//    val serverSocket = createSocket()
-//    val onMessageReceived: OnMessageReceived = { sourceHandler, message ->
-//        println(message)
-////        connections.filter {
-////            it != sourceHandler
-////        }.forEach {
-////            it.sendMessage(message)
-////        }
-//    }
-//
-//    if (serverSocket == null) {
-//        println("Can't start socket.")
-//        return
-//    } else {
-//        println("Socket started on port ${serverSocket.localPort}, ${serverSocket.inetAddress}")
-//    }
-//
-//    while (true) {
-//        val connectionSocket = serverSocket.accept()
-//        connections.add(ConnectionHandler(connectionSocket, onMessageReceived))
-//    }
-//}
-//
-//fun createSocket(): ServerSocket? {
-//    val bufferedReader = BufferedReader(InputStreamReader(System.`in`))
-//    println("Enter port number:")
-//
-//    val portString = bufferedReader.readLine()
-//    val port = Integer.parseInt(portString)
-//    var socket: ServerSocket? = null
-//
-//    try {
-//        socket = ServerSocket(port)
-//    } catch (e: Exception) {
-//        e.printStackTrace()
-//    }
-//
-//    return socket
-//}
-//
-//class ConnectionHandler(
-//    socket: Socket,
-//    private val onMessageReceived: OnMessageReceived
-//) {
-//    private val inputStream = socket.getInputStream()
-//    private val bufferedInputReader = inputStream.bufferedReader(Charsets.UTF_8)
-//    private val outputStream = socket.getOutputStream()
-//    private val bufferedWriter = outputStream.bufferedWriter()
-//
-//    val inputThread = Thread {
-//        while (true) {
-//            val input = bufferedInputReader.readLine()
-//            println(input)
-//        }
-//    }.also {
-//        it.start()
-//    }
-//
-//    fun sendMessage(line: String) {
-//        try {
-//            bufferedWriter.write(line + "\n")
-//            bufferedWriter.flush()
-//        } catch (e: IOException) {
-//            e.printStackTrace()
-//        }
-//    }
-//}
-
+import java.util.concurrent.Executors
 
 fun main() {
     val server = HttpServer.create(InetSocketAddress(8000), 0)
-    server.createContext("/test", MyHandler())
-    server.executor = null // creates a default executor
+    server.createContext("/files", Handler())
+    server.executor = Executors.newFixedThreadPool(2) // creates a default executor
     server.start()
 }
 
-internal class MyHandler : HttpHandler {
+class Handler : HttpHandler {
 
     override fun handle(exchange: HttpExchange) {
         println("Got request")
-        val response = "This is the response"
-        val method = exchange.requestMethod
-        when(method) {
-            "GET" -> processGet(exchange)
-            "PUT" -> processPut(exchange)
-            "POST" -> processPost(exchange)
-            "DELETE" -> processDelete(exchange)
-            "COPY" -> processCopy(exchange)
-            "MOVE" -> processMove(exchange)
+
+        val args = exchange.requestURI.query.split("&")
+
+        when (exchange.requestMethod) {
+            "GET" -> processGet(exchange, args)
+            "PUT" -> processPut(exchange, args)
+            "POST" -> processPost(exchange, args)
+            "DELETE" -> processDelete(exchange, args)
+            "COPY" -> processCopy(exchange, args)
+            "MOVE" -> processMove(exchange, args)
         }
+    }
 
-//        exchange.sendResponseHeaders(200, response.length.toLong())
-//        val os = exchange.responseBody
-//        os.write(response.toByteArray())
-//        os.close()
-        val query = exchange.requestURI.query
-        val path = query.substring(4)
-        val file = File(path)
-        val text = file.inputStream().readBytes()
-//        val text = "123"
-        exchange.sendResponseHeaders(200, text.size.toLong())
+    private fun processGet(exchange: HttpExchange, args: List<String>) {
+        try {
+            val path = args[0].substring(4)
+            val file = File(path)
+            if(!file.exists()) {
+                send404(exchange)
+                return
+            }
+            val text = file.inputStream().bufferedReader().readText()
+            exchange.sendResponseHeaders(200, text.length.toLong())
+            val os = exchange.responseBody
+            os.write(text.toByteArray())
+            os.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            exchange.sendResponseHeaders(500, 0)
+        }
+    }
+
+    private fun processPut(exchange: HttpExchange, args: List<String>) {
+        try {
+            val path = args[0].substring(4)
+            val text = args[1].substring(5)
+            val file = File(path)
+            if(!file.exists()) {
+                send404(exchange)
+                return
+            }
+
+            file.writeBytes(text.toByteArray())
+            exchange.sendResponseHeaders(200, 0)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            exchange.sendResponseHeaders(500, 0)
+        }
+    }
+
+    private fun processPost(exchange: HttpExchange, args: List<String>) {
+        try {
+            val path = args[0].substring(4)
+            val text = args[1].substring(5)
+            val file = File(path)
+            if(!file.exists()) {
+                send404(exchange)
+                return
+            }
+
+            file.appendBytes(text.toByteArray())
+            exchange.sendResponseHeaders(200, 0)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            exchange.sendResponseHeaders(500, 0)
+        }
+    }
+
+    private fun processDelete(
+        exchange: HttpExchange,
+        args: List<String>
+    ) {
+        try {
+            val path = args[0].substring(4)
+            val file = File(path)
+            if(!file.exists()) {
+                send404(exchange)
+                return
+            }
+            val result = file.delete()
+            if (result) {
+                exchange.sendResponseHeaders(200, 0)
+            } else {
+                exchange.sendResponseHeaders(500, 0)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            exchange.sendResponseHeaders(500, 0)
+        }
+    }
+
+    private fun processCopy(exchange: HttpExchange, args: List<String>) {
+        try {
+            val path = args[0].substring(4)
+            val srcFile = File(path)
+            val path2 = args[1].substring(4)
+            val dstFile = File(path2)
+
+            if(!srcFile.exists()) {
+                send404(exchange)
+                return
+            }
+
+            val result = srcFile.copyTo(dstFile, true)
+
+            if (result.exists()) {
+                exchange.sendResponseHeaders(200, 0)
+            } else {
+                exchange.sendResponseHeaders(500, 0)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            exchange.sendResponseHeaders(500, 0)
+        }
+    }
+
+    private fun processMove(exchange: HttpExchange, args: List<String>) {
+        try {
+            val path = args[0].substring(4)
+            val srcFile = File(path)
+            val path2 = args[1].substring(4)
+            val dstFile = File(path2)
+
+            val result = srcFile.copyTo(dstFile, true)
+            if(!srcFile.exists()) {
+                send404(exchange)
+                return
+            }
+
+            if (result.exists()) {
+                exchange.sendResponseHeaders(200, 0)
+            } else {
+                exchange.sendResponseHeaders(500, 0)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            exchange.sendResponseHeaders(500, 0)
+        }
+    }
+
+    private fun send404(exchange: HttpExchange) {
+        val message = "File not found"
+        exchange.sendResponseHeaders(404, message.length.toLong())
         val os = exchange.responseBody
-        os.write(text)
+        os.write(message.toByteArray())
         os.close()
-    }
-
-    private fun processGet(exchange: HttpExchange) {
-//        val query = exchange.requestURI.query
-//        val path = query.substring(4)
-//        val file = File(path)
-//        val text = file.inputStream().bufferedReader().readText()
-//        exchange.sendResponseHeaders(200, text.length.toLong())
-//        val os = exchange.responseBody
-//        os.write(text.toByteArray())
-//        os.close()
-    }
-
-    private fun processPut(exchange: HttpExchange) {
-
-    }
-
-    private fun processPost(exchange: HttpExchange) {
-
-    }
-
-    private fun processDelete(exchange: HttpExchange) {
-
-    }
-
-    private fun processCopy(exchange: HttpExchange) {
-
-    }
-
-    private fun processMove(exchange: HttpExchange) {
-
     }
 }
